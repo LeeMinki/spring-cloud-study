64998fc4ab9877e43c4c1f1af9471bbe620a6d77
leeminki <curyowo@gmail.com>
2022-12-19 16:05:37 +0900

    [Service Discovery] User Service - 프로젝트 생성

--- user-service/src/test/java/com/example/userservice/UserServiceApplicationTests.java
+++ user-service/src/test/java/com/example/userservice/UserServiceApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.ecommerce;
+package com.example.userservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class EcommerceApplicationTests {
+class UserServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
